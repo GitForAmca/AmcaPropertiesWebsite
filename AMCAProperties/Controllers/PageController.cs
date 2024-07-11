00e739e150508079ca2bfa91d2a8461dbf86b198
Mohd Owais Ansari <it1@amca.ae>
2024-07-11 15:07:43 +0530

    files added

---
+++ AMCAProperties/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMCAProperties.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about-us")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact-us")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ AMCAProperties/Scripts/custom.js
@@ -0,0 +1,56 @@
+﻿/*item Slider*/
+$(document).ready(function () {
+    $(".itemSlider").owlCarousel({
+        items: 3,
+        autoplay: false,
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                loop: true,
+                margin: 0,
+                nav: false,
+                items: 2,
+                left: 70
+            },
+            600: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            1366: {
+                items: 3,
+            },
+            1440: {
+                items: 3,
+            },
+            1680: {
+                items: 3,
+            },
+        }
+    });
+});
+
+/*Testimnilas Slider*/
+$('.testimonials-cr').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    item: 3,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
